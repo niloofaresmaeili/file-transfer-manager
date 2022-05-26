9deb1a4ae7287b4f301e6acf42d3777d51f54990
niloufaresmaeili <niloufaresmaeili98@gmail.com>
2022-05-26 16:13:29 +0430

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import { app } from './app'
 const port = 5000
 
 // Start server
-app.listen(port, () => console.log(`File Transfer manager application is running on port ${port}!`))
+app.listen(port, () => console.log(`File Transfer Manager application is running on port ${port}!`))
