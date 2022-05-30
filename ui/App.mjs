dc00f8c60e8b915902f606fb067409f85b8af8a6
easa <eisanodehi@gmail.com>
2022-05-30 12:25:09 +0430

    seprate header component

--- ui/App.mjs
+++ ui/App.mjs
@@ -1,6 +1,7 @@
 import { h } from 'https://unpkg.com/preact@latest?module';
 import htm from 'https://unpkg.com/htm?module';
 import Files from '/component/Files.mjs'
+import Header from '/component/Header.mjs'
 import useApp from './useApp.mjs'
 
 const html = htm.bind(h);
@@ -18,20 +19,14 @@ export default function App() {
 
   return html`
   <div class="container-xl">
-    <header class="py-3 mb-3 border-bottom">
-      <div class="container-fluid d-grid gap-3 align-items-center" style="grid-template-columns: 1fr;">
-        <div class="input-group mb-3">
-          <button onClick=${goBack} class="input-group-text" id="basic-addon1">back</button>
-          <input value=${pathName} onChange=${setPathName} class="form-control" aria-describedby="basic-addon1" type="search" placeholder="path..." aria-label="Search" />
-        </div>
-      </div>
-    </header>
+
+    <${Header} goBack=${goBack} pathName=${pathName} setPathName=${setPathName} />
    
     <main>
-      <div class="row">
+
         <${Files} files=${directories} onClick=${loadDirectory} />
         <${Files} files=${files} onClick=${downloadFile} />
-      </div>
+
     </main>
   </div>
     `;
